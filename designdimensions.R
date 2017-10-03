44283fd30a40fd2d6c4561a1703e9886cd682315
Agile Bean <chaehan.so@gmail.com>
2017-10-03 19:35:36 +0900

    added trainNeuralNetwork, print predictions,  get_data > read_machinelearning_data

--- designdimensions.R
+++ designdimensions.R
@@ -1,158 +1,175 @@
-#################################################################################################
+###################################################################################################
+rm(list=ls())
+library(tidyr)
+library(dplyr)
+library(magrittr)
+library(neuralnet)
 library(NeuralNetTools)
 
-# create model
-library(neuralnet)
-AND <- c(rep(0, 7), 1)
-OR <- c(0, rep(1, 7))
-binary_data <- data.frame(expand.grid(c(0, 1), c(0, 1), c(0, 1)), AND, OR)
-binary_data
-mod <- neuralnet(AND + OR ~ Var1 + Var2 + Var3, binary_data,
-                 hidden = c(6, 12, 8), rep = 10, err.fct = 'ce', linear.output = FALSE)
+source("/Users/Chaehan/Dropbox/01 IDAS Prof/06 Data Analysis/06 R Scripts/utils.R")
+dir <- "/Users/Chaehan/Dropbox/01 IDAS Prof/06 Data Analysis/06 Machine Learning"
+setwd(dir)
 
-# plotnet
-par(mar = numeric(4), family = 'serif')
-plotnet(mod, alpha = 0.6)
+######################################################################
 
-#################################################################################################
 
-# EXAMPLE MASS:Boston data
-# SOURCE https://www.r-bloggers.com/fitting-a-neural-network-in-r-neuralnet-package/
+trainNeuralNetwork <- function(DV, formula, training_set, testing_set, layers, mode, palette)
+{
+  ######################################################################
+  # Linear Regression
+  #
+  lm.fit <- lm(formula, data=training_set)
+  summary(lm.fit)
+  predicted.lm <- predict(lm.fit,testing_set) # vector
+  MSE.lm <- (sum((predicted.lm - testing_set[DV])^2)/nrow(testing_set) )
+  
+  ######################################################################
+  # Neural Network
+  # trainingset must only contain same variables as testing set, NOT MORE!
+  nn <- neuralnet(formula,training_set,hidden=layers,linear.output=FALSE)
+  # Compute Predictions from testing set. MUST remove DV!!
+  predicted.values <- compute(nn,testing_set[-1])
+  
+  observed.neural <- testing_set[DV] # dataframe
+  predicted.neural <- predicted.values$net.result # matrix
+  
+  # cbind(observed, predicted, (observed-predicted)/observed) %>% round(digits=2) %>% print
+  # table(observed,predicted)
+  
+  # observed.neural.normal <- (testing_set$NPS)*(max(data$NPS)-min(data$NPS))+min(data$NPS)
+  # predicted.neural.normal <- predicted.values$net.result*(max(data$NPS)-min(data$NPS))+min(data$NPS)
+  # MSE.nn <- sum( ((observed.neural.normal - predicted.neural.normal)^2)/nrow(predicted.neural.normal))
+  
+  MSE.nn <- sum( ((observed.neural - predicted.neural)^2)/nrow(predicted.neural))
+  
+  c(MSE.lm,MSE.nn) %>% round(digits = 3) %>% print
+  
+  ######################################################################
+  # Visualize Prediction Quality: NN vs. LM
+  #
+  ### ifelse does NOT work if ggplot object is returned!!! ###
+  # pred <- ifelse(mode == "integrate", 
+  #        print_both_predictions(observed.neural, predicted.neural, predicted.lm, palette),
+  #        print_separate_predictions(observed.neural, predicted.neural, predicted.lm, palette)
+  # )
+  
+  if(mode == "integrate") 
+  {
+    pred <- print_both_predictions(observed.neural, predicted.neural, predicted.lm, palette)
+  } else {
+    pred <- print_separate_predictions(observed.neural, predicted.neural, predicted.lm, palette)
+  }
+  
+  ######################################################################
+  # plot neural net with NeuralNetTools
+  require(NeuralNetTools)
+  plotnet(nn)
+  
+  return(list(nn, pred))
+}
+
+
+print_both_predictions <- function(DV, predicted_nn, predicted_lm, palette_label) {
+  require(ggplot2)
+  # coerce into dataframe: df, matrix, vector!
+  data <- cbind(DV, predicted_nn, predicted_lm) %>% 
+    as.data.frame %>% setNames(., c("observed", "predicted.nn", "predicted.lm"))
+  
+  gg <- ggplot(data, mapping = aes(x=observed)) +
+    ggtitle("Observed vs. Predicted Values") +
+    ylab("predicted") +
+    geom_point(aes(y=predicted_lm, color="linear")) +
+    geom_point(aes(y=predicted_nn, color="neural")) +
+    geom_smooth(method=lm, aes(y=predicted_lm, color="linear")) +
+    geom_smooth(method=loess, aes(y=predicted_nn, color="neural")) +
+    scale_color_brewer(palette = palette_label) +
+    theme(legend.title = element_text(margin = 0))
+  
+  # print(gg)
+  return(gg)
+}
+
+print_separate_predictions <- function(DV, predicted_nn, predicted_lm, palette_label) {
+  require(ggplot2)
+  data <- cbind(DV, predicted_nn, predicted_lm) %>% 
+    as.data.frame %>% setNames(., c("observed", "predicted.nn", "predicted.lm"))
+  
+  plot.base <- ggplot(data, mapping = aes(x = observed)) +
+    ggtitle("Observed vs. Predicted Values") +
+    scale_color_brewer(palette = palette_label)
+  
+  gg1 <- ggplot(data, mapping = aes(x=observed)) +
+    geom_point(aes(y=predicted.nn, color="neural")) +
+    geom_smooth(method=lm, aes(y=predicted.nn, color="neural"))
+  
+  gg2 <- ggplot(data, mapping = aes(x=observed)) +
+    geom_point(aes(y=predicted.lm, color="linear")) +
+    geom_smooth(method=lm, aes(y=predicted.lm, color="linear"))
+  
+  require(cowplot)
+  plot_grid(gg1, gg2) %T>% print %>% return
+  
+}
+
+
+######################################################################
 #
-set.seed(500)
-library(MASS)
-data <- Boston
+# Collapse raw data to feature set 
+#
+######################################################################
 
-# check for missing data
-apply(data,2,function(x) sum(is.na(x)))
+get_data <- function(DV, split_ratio)
+{
+  # get data
+  features  <- read_machinelearning_data("NPS") %>% .[[2]]
+  data      <- read_machinelearning_data("NPS") %>% .[[1]] %>% 
+    select(., one_of(c(DV, features)))
+  
+  # create formula from features
+  formula <- features %>% paste (., collapse=" + ") %>% paste(DV, "~ ", .) %>%
+    as.formula(env= .GlobalEnv)
+  
+  # get max and min of columns for scaling
+  maxs <- data %>% apply(., 2, max)
+  mins <- data %>% apply(., 2, min)
+  
+  # Use scale() and convert the resulting matrix to a data frame
+  data.scaled <- as.data.frame(scale(data,center = mins, scale = maxs - mins))
+  
+  # Create Split (any column is fine)
+  require(caTools)
+  set.seed(101)
+  split <-  sample.split(data.scaled[,1], SplitRatio = split_ratio)
+  
+  # Split based off of split Boolean Vector
+  trainingset <-  subset(data.scaled, split == TRUE)  
+  testingset  <-  subset(data.scaled, split == FALSE)
+  
+  return(list(formula, trainingset, testingset))
+}
 
-# sample(x, size) := create training set randomly from 75% (size) of the data indices (1:nrow)
-index <- sample(1:nrow(data),round(0.75*nrow(data)))
-# use the randomly drawn indices to define training set
-train <- data[index,]
-test <- data[-index,]
+main <- function(DV, hidden_layers, mode, palette)
+{
+  
+  data <- get_data(DV, split_ratio = 0.70)
+  result <- trainNeuralNetwork(DV, formula = data[[1]], 
+                               training_set = data[[2]], testing_set = data[[3]], 
+                               hidden_layers, 
+                               mode, palette) # Paired, RdBu
+  return(result)
+}
 
-# linear regression
-lm.fit <- glm(medv~., data=train)
-summary(lm.fit)
-pr.lm <- predict(lm.fit,test)
+# first run
+system.time(
+  result <- main("NPS", 
+                 hidden_layers=c(3,3,3), 
+                 mode="integrate", 
+                 palette = "RdBu") # Paired, RdBu
+)
 
-# MSE = mean squared error to evaluate regression
-MSE.lm <- sum((pr.lm - test$medv)^2)/nrow(test)
+# prediction quality
+result[[2]] 
++ scale_color_brewer("Set1")
+# neural network
+result[[1]] %>% plotnet 
 
-# data prep
-maxs <- apply(data, 2, max) 
-mins <- apply(data, 2, min)
-
-scaled <- as.data.frame(scale(data, center = mins, scale = maxs - mins))
-
-train_ <- scaled[index,]
-test_ <- scaled[-index,]
-
-
-library(neuralnet)
-n <- names(train_)
-f <- as.formula(paste("medv ~", paste(n[!n %in% "medv"], collapse = " + ")))
-nn <- neuralnet(f,data=train_,hidden=c(5,3),linear.output=T)
-
-plot(nn)
-
-# PREDICTION
-
-pr.nn <- compute(nn,test_[,1:13])
-
-pr.nn_ <- pr.nn$net.result*(max(data$medv)-min(data$medv))+min(data$medv)
-test.r <- (test_$medv)*(max(data$medv)-min(data$medv))+min(data$medv)
-
-MSE.nn <- sum((test.r - pr.nn_)^2)/nrow(test_)
-
-print(paste(MSE.lm,MSE.nn))
-
-par(mfrow=c(1,2))
-plot(test$medv,pr.nn_,col='red',main='Real vs predicted NN',pch=18,cex=0.7)
-abline(0,1,lwd=2)
-legend('bottomright',legend='NN',pch=18,col='red', bty='n')
-
-plot(test$medv,pr.lm,col='blue',main='Real vs predicted lm',pch=18, cex=0.7)
-abline(0,1,lwd=2)
-legend('bottomright',legend='LM',pch=18,col='blue', bty='n', cex=.95)
-
-par("mar")
-par(mfrow=c(1,1))
-# more intuitive plot
-plot(test$medv,pr.nn_,col='red',main='Real vs predicted NN',pch=18,cex=0.7)
-points(test$medv,pr.lm,col='blue',pch=18,cex=0.7)
-abline(0,1,lwd=2)
-legend('bottomright',legend=c('NN','LM'),pch=18,col=c('red','blue'))
-
-###################################################################################################
-
-library(neuralnet)
-data(iris)
-
-ind <- sample(2, nrow(iris), replace = TRUE, prob=c(0.7, 0.3))
-
-
-trainset$setosa = trainset$Species == "setosa"
-trainset$virginica = trainset$Species == "virginica"
-trainset$versicolor = trainset$Species == "versicolor"
-
-trainset = iris[ind == 1,]> testset = iris[ind == 2,]
-
-
-###################################################################################################
-library(ISLR)
-names(College)
-
-data <- College[,-1]
-maxs <- data %>% apply(., 2, max)
-mins <- data %>% apply(., 2, min)
-
-# Use scale() and convert the resulting matrix to a data frame
-scaled.data <- as.data.frame(scale(data,center = mins, scale = maxs - mins))
-scaled.data
-# Check out results
-print(head(scaled.data,2))
-
-Private = as.numeric(College$Private)-1
-data = cbind(Private,scaled.data)
-
-library(caTools)
-set.seed(101)
-
-# Create Split (any column is fine)
-split = sample.split(data$Private, SplitRatio = 0.70)
-
-# Split based off of split Boolean Vector
-train = subset(data, split == TRUE)
-test = subset(data, split == FALSE)
-
-names(train)
-
-features <- names(scaled.data)
-
-features
-
-# Concatenate strings
-f <- features %>% paste (., collapse=" + ") %>% paste("Private ~ ", .) %>% 
-  as.formula(env= .GlobalEnv)
-f
-
-library(neuralnet)
-nn <- neuralnet(f,train,hidden=c(10,10,10),linear.output=FALSE)
-
-# Compute Predictions off Test Set
-predicted.values <- compute(nn,test[-1])
-
-# Check out net.result
-print(head(predicted.values$net.result))
-
-# round
-predicted.values$net.result <- sapply(predicted.values$net.result,round,digits=0)
-
-test$Private
-predicted.values$net.result
-
-table(test$Private,predicted.values$net.result)
-
-plot(nn)
